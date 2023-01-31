a734fa66afccfb1442761c503fd5157da8297265
Cameron Gunter <crmgunter@gmail.com>
2023-01-31 03:12:07 -0500

    add respawn when player falls below y axis threshold

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,11 +1,14 @@
 extends KinematicBody2D
 
+var starting_pos
 var speed = 400
 var jump_speed = -275
 var gravity = 200
 var velocity = Vector2()
+onready var player = get_node("Sprite")
 
 func get_input(delta):
+	print(starting_pos, position)
 	velocity.x = 0
 	
 	if Input.is_action_just_pressed("sprint") and (is_on_floor()):
@@ -20,10 +23,22 @@ func get_input(delta):
 		if (is_on_floor()):
 			velocity.y += jump_speed
 	if (not is_on_floor()):
-		velocity. y -= (jump_speed * 1.25) * delta
+		velocity.y -= (jump_speed * 1.25) * delta
+		
+	if velocity.y > 1000:
+		velocity.y = 0
+		velocity.x = 0
 	
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
+	var pos = player.global_position
+	if pos.y > 1000:
+		position = starting_pos
+		print("fuck")
+	
 func _physics_process(delta):
 	get_input(delta)
+
+func _ready():
+	starting_pos = position
